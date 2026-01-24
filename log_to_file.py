a3a402bcdac06f42f58b0a0ea336579067d979ae
Bhaskar <tvbchary@gmail.com>
2026-01-25 01:29:31 +0530

    math, datetime modules updated

---
+++ log_to_file.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logging.info("App started")
+logging.error("API call failed")
